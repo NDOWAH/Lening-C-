00d08afc8b532bdf2bb832d30605df56ad996647
Marcel grossmann <marcelgrossmann80@gmail.com>
2024-01-02 21:39:21 +0100

    add product function to calculate the product of two numbers

--- main.cpp
+++ main.cpp
@@ -3,11 +3,18 @@ int addNumbers(int firt_param, int second_param){
     int sum = firt_param + second_param;
     return sum;
 }
+int multiplyIntegers(int first_param, int second_param){
+    int result = first_param * second_param;
+    return result;
+    }
+
 int main(){
     int first_number{10};
     int second_number{29};
-    int sum;
+    int sum, product;
     sum = addNumbers(first_number, second_number);
-    std:: cout <<"The sum of the two numbers is:" <<sum << std:: endl;
+    product = multiplyIntegers(first_number, second_number);
+    std:: cout <<"The sum of the two numbers is:" <<sum
+               <<"and their product is: "<<product << std::endl;
 
 }
