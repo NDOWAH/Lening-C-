a54f45d94d1437b9116f668b4d3e0334b59a8ad1
Marcel grossmann <marcelgrossmann80@gmail.com>
2024-01-02 20:50:49 +0100

    add two numbers in c++

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    int first_number{10};
+    int second_number{29};
+    int sum;
+    std::cout <<"The first number is: "<<first_number<<std::endl;
+    sum = first_number + second_number;
+    std:: cout <<"The sum of the two numbers is: " <<sum << std:: endl;
+
+}
