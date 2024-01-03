21cd867d63a25d3759aaaa9e282077edd064810b
Marcel grossmann <marcelgrossmann80@gmail.com>
2024-01-03 20:51:33 +0100

    modify main program to take in put from user

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+
+int first_number{10};
+int second_number{29};
+
 int addNumbers(int firt_param, int second_param){
     int sum = firt_param + second_param;
     return sum;
@@ -9,12 +13,16 @@ int multiplyIntegers(int first_param, int second_param){
     }
 
 int main(){
-    int first_number{10};
-    int second_number{29};
+    int first_number;
+    int second_number;
+    std:: cout <<"What is your first number: ";
+    std:: cin >> first_number;
+    std:: cout << "What is the second number: ";
+    std:: cin >>second_number;
     int sum, product;
     sum = addNumbers(first_number, second_number);
     product = multiplyIntegers(first_number, second_number);
     std:: cout <<"The sum of the two numbers is:" <<sum
-               <<"and their product is: "<<product << std::endl;
+               <<" and their product is: "<<product << std::endl;
 
 }
