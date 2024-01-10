dbfe83bd5755121e10250bd1694f514f91936fe7
Marcel grossmann <marcelgrossmann80@gmail.com>
2024-01-10 22:20:02 +0100

    experiment with flush

--- formatting.cpp
+++ formatting.cpp
@@ -1,6 +1,7 @@
 # include<iostream>
 
 int main(){
+    //endl give the option to move to a new line
     //Experimentation without endl from iostream library
     std::cout <<"Hello Jane ";
     std::cout <<"how are you doing?\n";
@@ -9,5 +10,10 @@ int main(){
     std::cout <<"Hello Jane " << std::endl;
     std::cout <<"how are you doing?"<<std::endl;
 
+   //========================================================
+   //flush gives the option to send data directly to the terminal without passing through buffer
+   //Experiement with flush from iostream library, no difference interm of display on how flush works unless behind the scences
+   std::cout << "++++++++++++++++++++++++++++++++++++++++++++\n\n";
+   std::cout <<"This is a begginner level C++ programming"<<std::endl<<std::flush;
 
 }
