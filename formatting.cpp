e1c57abee3fb0f07ab9308466e149991238392b2
Marcel grossmann <marcelgrossmann80@gmail.com>
2024-01-10 22:06:17 +0100

    format with endl

---
+++ formatting.cpp
@@ -0,0 +1,13 @@
+# include<iostream>
+
+int main(){
+    //Experimentation without endl from iostream library
+    std::cout <<"Hello Jane ";
+    std::cout <<"how are you doing?\n";
+    std::cout <<"=========================================\n\n";
+    //With endl
+    std::cout <<"Hello Jane " << std::endl;
+    std::cout <<"how are you doing?"<<std::endl;
+
+
+}
