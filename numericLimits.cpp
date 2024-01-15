7db11eb24f7138578e6067ab2463afdca34d9a58
Marcel grossmann <marcelgrossmann80@gmail.com>
2024-01-15 20:02:49 +0100

    add numeric limits

---
+++ numericLimits.cpp
@@ -0,0 +1,7 @@
+# include <iostream>
+# include <limits>
+
+int main(){
+    std::cout <<"The range for short form is from"<<std::numeric_limits<short>::min()
+    <<"to"<<std::numeric_limits<short>::max()<<std::endl;
+}
