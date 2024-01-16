54a4ace5b0c9209d9225a1fefd91fe8f5b373742
Marcel grossmann <marcelgrossmann80@gmail.com>
2024-01-16 20:42:57 +0100

    test math functions in cmath labrary

---
+++ mathFunctions.cpp
@@ -0,0 +1,20 @@
+# include <iostream>
+# include <cmath>
+
+int main(){
+    float weight{60.9875345};
+    int base{2};
+    int power{9};
+    int num{-35};
+    std::cout <<std::floor(weight)<<std::endl;
+    std::cout <<std::ceil(weight)<<std::endl;
+    std::cout <<"===============================\n\n";
+    std::cout <<std::pow(2,5)<<std::endl;
+    std::cout <<std::abs(num)<<std::endl;
+    std::cout <<std::exp(base)<<std::endl;
+    std::cout <<std::round(weight)<<std::endl;
+    std::cout <<std::log(9)<<std::endl;
+
+    
+
+}
