230c7a325273b4b4e967f3213e55c8f9cfca1f44
Marcel grossmann <marcelgrossmann80@gmail.com>
2024-01-21 00:12:43 +0100

    add for loop

--- loops/forLoop.cpp
+++ loops/forLoop.cpp
@@ -12,7 +12,12 @@ Body
 */
 int main(){
 const size_t count{10};
-for (int i{}; i< count; i++)
+/*for (int i{}; i< count; i++)
     std::cout<<"The value if i is: "<<i<<std::endl;
-
+    */
+int books_count{};
+for (; books_count<count; books_count++){
+std::cout<<books_count<<": Books in the library"<<std::endl;
+}
+std::cout<<"The total number of books is: "<< books_count<<std::endl;
 }
