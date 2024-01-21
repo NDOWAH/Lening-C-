20f4ec0cf40778ba5fccab91dcb89cf53b106ee4
Marcel grossmann <marcelgrossmann80@gmail.com>
2024-01-21 21:46:02 +0100

    remove comment

--- arrays/arrayBasics.cpp
+++ arrays/arrayBasics.cpp
@@ -1,5 +1,4 @@
 # include <iostream>
-//one integer-->4bytes, 5 int--> 20 bytes therefore size of marks --20byts.
 int main(){
     int marks[5] {15, 18, 14, 19, 20};
     for (int i =0; i<sizeof(marks)/4; i++){
