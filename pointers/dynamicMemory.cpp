5d9003eea4666f0f8c0a05bd2d7f45c1891a2c9b
Marcel grossmann <marcelgrossmann80@gmail.com>
2024-01-22 22:29:09 +0100

    test bad coding habit

--- pointers/dynamicMemory.cpp
+++ pointers/dynamicMemory.cpp
@@ -15,19 +15,20 @@ int main(){
     std::cout<<"Writing into uninitialized pointer"<<std::endl;
     std::cout<<"p_number points to: "<<p_number<<" address"<<std::endl;
     std::cout<<"The dereferenced value is: "<<*p_number<<std::endl;
+     */
     std::cout<<"====================================================\n\n";
-    */
+   
 /*The statement below initializes a pointer to null
 it is equavalent to *p_number1{nullptr}
-This indicates that the pointer points to nowhere*/
-
+This indicates that the pointer points to nowhere
+The program below crashes because we are trying to use
+memory that is not ours.*/
+   std::cout<<"The program below crashes"<<std::endl;
     int *p_number1{};//
     *p_number1 = 54; //Writing to a pointer that points to nowhere: Bad practice.
     std::cout<<"Reading and writing to null pointers"<<std::endl;
     std::cout<<"The pointer *p_number1 points to: "<<p_number1<<" address"<<std::endl;
     std::cout<<"Its value is: "<<*p_number1<<std::endl;
-
-
 
     return 0;
 }
