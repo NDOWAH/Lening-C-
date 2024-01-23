6f1868b4e557519dc6835f5a53d68e0ca9c4b9fa
Marcel grossmann <marcelgrossmann80@gmail.com>
2024-01-23 21:57:37 +0100

    add deleted dangling pointer

--- pointers/danglingPointer.cpp
+++ pointers/danglingPointer.cpp
@@ -10,9 +10,18 @@ Multiple pointers pointing to the same memory allocation.
 
 int main(){
    int *p_NUMBER;
-   std::cout<<"Case1: Uninitialized dangling pointer\n";
+   int *p_NUMBER1{new int{29}};
+   std::cout<<"=================Case1:Uninitialized dangling pointers==========================\n\n";
    std::cout<<"The address of p_NUMBER is: "<<p_NUMBER<<std::endl;
    std::cout<<"The current value of p_NUMBER is: "<<*p_NUMBER<<std::endl;
+   std::cout<<"Case1: Uninitialized dangling pointer\n";
+   std::cout<<"The address of p_NUMBER is: "<<p_NUMBER1<<std::endl;
+   std::cout<<"The current value of p_NUMBER is: "<<*p_NUMBER1<<std::endl;
+   std::cout<<"=================Case2:deleted dangling pointers==========================\n\n";
+
+   delete p_NUMBER1;
+   std::cout<<"The address of the deleted pointer p_NUMBER is: "<<p_NUMBER1<<std::endl;
+   std::cout<<"The current value of the deleted pointer p_NUMBER is: "<<*p_NUMBER1<<std::endl;
 
 }
 
