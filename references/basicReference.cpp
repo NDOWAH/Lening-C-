eacb6d6d303b3c1eddac147b5e210e4837e1a097
Marcel grossmann <marcelgrossmann80@gmail.com>
2024-01-26 22:37:37 +0100

    add comment about references

--- references/basicReference.cpp
+++ references/basicReference.cpp
@@ -1,7 +1,10 @@
 # include <iostream>
 
 /*Referencing is a way of setting up aliases for variables. 
-With this aliases we can modify the variables with them*/
+With this aliases we can modify the variables with them
+---References can not be change to reference something else opposed to pointers
+---they must be initialized at declaration not in the case of pointers
+---not advicable to use references for reading and writting*/
 int main(){
     int value{30};
     double double_value{235543};
