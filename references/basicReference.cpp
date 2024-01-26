4bad6d880cbaa38bdfac0914da8ed7f6aa4f5cbd
Marcel grossmann <marcelgrossmann80@gmail.com>
2024-01-26 21:57:18 +0100

    use actual variable to change reference variable and vice versa

--- references/basicReference.cpp
+++ references/basicReference.cpp
@@ -12,4 +12,21 @@ int main(){
     <<&value<<std::endl;
     std::cout<<"new_double_value: "<<new_double_value<<" address: "<<&new_double_value<<" address of value "
     <<&double_value<<std::endl;
+    std::cout<<"==============================================\n\n";
+    //changing actual values to see effects on references
+    value = 50;
+    double_value = 111;
+    std::cout<<"New_value: "<<new_value<<" address: "<<&new_value<<" address of value: "
+    <<&value<<std::endl;
+    std::cout<<"new_double_value: "<<new_double_value<<" address: "<<&new_double_value<<" address of value "
+    <<&double_value<<std::endl;
+    std::cout<<"++++++++++++++++++++++++++++++++++++++++++++++\n\n";
+    //use reference to change the value if reference variable
+    new_value = 90;
+    new_double_value = 120;
+    std::cout<<"New_value: "<<new_value<<" address: "<<&new_value<<" address of value: "
+    <<&value<<std::endl;
+    std::cout<<"new_double_value: "<<new_double_value<<" address: "<<&new_double_value<<" address of value "
+    <<&double_value<<std::endl;
+
 }
