5d242bf88d09ada8e7512d048aa5a17f41243ab4
Marcel grossmann <marcelgrossmann80@gmail.com>
2024-01-31 21:24:15 +0100

    refactor code

--- strings/stringManipulation.cpp
+++ strings/stringManipulation.cpp
@@ -1,16 +1,22 @@
 # include <iostream>
 # include <string>
+using namespace std;
 
 int main(){
     char message[]{"Hello Jane, how are you doing?"};
     char *message_ptr[]{message};
-   
-    std::cout<<"The message length is: "<<std::strlen(message)<<std::endl;
-    std::cout<<"The message pointer length is: "<<std::strlen(*message_ptr)<<std::endl;
+    string city{"Alabama"};
+    string city1{"Blabama"};
+    cout<<"The message length is: "<<strlen(message)<<endl;
+    cout<<"The message pointer length is: "<<strlen(*message_ptr)<<endl;
 
     /*The sizeof function can be use in the same way but 
     it would include the null character as opposed by strlen function
     The sizeof can't be application in pointer*/ 
-    std::cout<<"The message length is: "<<sizeof(message)<<std::endl;  //gives 31  
-    std::cout<<"The message length is: "<<sizeof(*message_ptr)<<std::endl;  
+    cout<<"The message length is: "<<sizeof(message)<<endl;  //gives 31  
+    cout<<"The message length is: "<<sizeof(*message_ptr)<<endl;  
+    cout<<std::boolalpha;
+    cout<<std::strcmp(city1, city)<<std::endl;
+    cout<<"Concatenate the strings: "<<std::strcat(city, city1)<<std::endl;
+    cout<<"Concatenate the strings: "<<std::strcat(city, city1)<<std::endl;
 }
