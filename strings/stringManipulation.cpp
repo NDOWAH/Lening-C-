0e3fe91a02b891c6f15045c10bc52dac4efe8975
Marcel grossmann <marcelgrossmann80@gmail.com>
2024-02-02 00:07:39 +0100

    add line separation

--- strings/stringManipulation.cpp
+++ strings/stringManipulation.cpp
@@ -9,13 +9,13 @@ int main(){
     string city1{"Blabama"};
     cout<<"The message length is: "<<strlen(message)<<endl;
     cout<<"The message pointer length is: "<<strlen(*message_ptr)<<endl;
+    cout<<"===================================================\n"<<endl;
 
     /*The sizeof function can be use in the same way but 
     it would include the null character as opposed by strlen function
     The sizeof can't be application in pointer*/ 
     cout<<"The message length is: "<<sizeof(message)<<endl;  //gives 31  
     cout<<"The message length is: "<<sizeof(*message_ptr)<<endl;  
-    cout<<std::boolalpha;
     cout<<std::strcmp(city1, city)<<std::endl;
     cout<<"Concatenate the strings: "<<std::strcat(city, city1)<<std::endl;
     cout<<"Concatenate the strings: "<<std::strcat(city, city1)<<std::endl;
